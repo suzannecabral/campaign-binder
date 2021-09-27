37f8dbb0d60643f02746ac7da85cb4738b1fee38
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-09-27 12:58:30 -0700

    fix page actions

--- src/actions/pageActions.js
+++ src/actions/pageActions.js
@@ -1,5 +1,5 @@
 import * as types from "../constants/actionTypes.js";
 
 export const getPages = () => (dispatch) => {
-	dispatch({ type: types.ADD_PAGE, payload: content });
+	dispatch({ type: types.ADD_PAGE /*, payload: content */ });
 };
